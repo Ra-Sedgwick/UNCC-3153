5f00deb03ad5191282e38e1244e26cfc8081d24f
Robert Adam Sedgwick <RaSedgwick.gmail.com>
2017-09-06 00:37:11 -0400

    Initail: BuildBoard() working

---
+++ 8-Queens/8-Queens/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Queens
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] board = BuildBoard(8, 8);
+            PrintGrid(board);
+        }
+
+        private static int[,] BuildBoard(int row, int col)
+        {
+            int[,] board = new int[row, col];
+            Random rand = new Random();
+
+
+            // Initialzie board with all 0's.
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < col; j++)
+                    board[i, j] = 0;
+
+            // Place One queen randomly in each column.
+            for (int i = 0; i < col; i++)
+            {
+                int queen = rand.Next(0, col);
+                Console.WriteLine(queen);
+                board[queen, i] = 1;
+            }
+
+            return board;
+        }
+
+        private static void PrintGrid(int[,] grid)
+        {
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                    Console.Write(grid[i, j] + " ");
+
+                Console.WriteLine();
+            }   
+        }
+
+    }
+
+}
---
+++ 8-Queens/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d303f003-aff4-4ba1-9b00-8e63c84e0767")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
